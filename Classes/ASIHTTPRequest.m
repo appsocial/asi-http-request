f457893b58f4a24d5ac85f12c0f5f5d7e4373822
Ianiv Schweber <ianiv@appsocial.ca>
2011-04-05 11:24:08 -0700

    Fix for "The left operand to '+' is always 0" warning

--- Classes/ASIHTTPRequest.m
+++ Classes/ASIHTTPRequest.m
@@ -4511,11 +4511,9 @@ static NSOperationQueue *sharedQueue = nil;
     NSMutableData* data = [NSMutableData dataWithLength:((length + 2) / 3) * 4];
     uint8_t* output = (uint8_t*)data.mutableBytes;
 	
-	NSInteger i;
-    for (i=0; i < length; i += 3) {
+    for (NSInteger i = 0; i < length; i += 3) {
         NSInteger value = 0;
-		NSInteger j;
-        for (j = i; j < (i + 3); j++) {
+        for (NSInteger j = i; j < (i + 3); j++) {
             value <<= 8;
 			
             if (j < length) {
